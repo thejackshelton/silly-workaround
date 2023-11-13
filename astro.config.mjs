276748c12912b006967af0ca0148ceb0ce5d2f9e
thejackshelton <me@jackshelton.com>
2023-11-12 22:59:02 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import qwikdev from '@qwikdev/astro';
+import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
